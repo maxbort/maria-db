caef414596a1e91e4e88c6a68a847326d1e88b5b
CHOI HyoJun <cjfgh2449@gmail.com>
2024-05-16 15:23:40 +0900

    feat. jdbc driver 추가

---
+++ jdbc-practices/src/main/java/test/ConnectionTest.java
@@ -0,0 +1,39 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Connection connection = null;
+		// 1. JDBC Driver 로딩
+		try {
+			
+			// 2. 연결하기
+			Class.forName("org.mariadb.jdbc.Driver");
+			String url = "jdbc:mariadb://192.168.0.198:3306/webdb?charset=utf-8";
+			connection = DriverManager.getConnection(url, "webdb","webdb");
+			
+			System.out.println("Connection success!!");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("드라이버 로딩 실패: " + e);
+		} catch (SQLException e) {
+			System.out.println("Error: " + e);
+		} finally {
+			try {
+				if(connection != null) {
+					connection.close();
+				}
+			} catch(SQLException e) {
+				System.out.println("Error: " + e);
+
+			}
+		}
+		
+	}
+
+}
